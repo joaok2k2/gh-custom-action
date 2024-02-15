db346844e758bc6671c883131e74c9d5d193f8b1
joaok2k2 <joak2k2@gmail.com>
2024-02-15 01:45:17 -0300

    Added output in custom action JavaScript s3 website url

--- .github/workflows/actions/deploy-s3-javascript/main.js
+++ .github/workflows/actions/deploy-s3-javascript/main.js
@@ -3,16 +3,17 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
+  // 1) Get some input values
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const disFolder = core.getInput("dist-folder", { required: true });
 
-    // 1) Get some input values
-    const bucket = core.getInput('bucket', { required: true })
-    const bucketRegion = core.getInput('bucket-region', { required: true })
-    const disFolder = core.getInput('dist-folder', { required: true })
+  // 2|) Upload files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${disFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    // 2|) Upload files
-    const s3Uri = `s3://${bucket}`
-    exec.exec( `aws s3 sync ${disFolder} ${s3Uri} --region ${bucketRegion}`)
-    core.notice('Hello from my custom JavaScript Actions!');
+  const websiteURL = `http:${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("s3-website-url", websiteURL); // set output
 }
 
 run();
