36780a5ecb18d5baf16b6dbaa2e58d793d29138e
joaok2k2 <joak2k2@gmail.com>
2024-02-15 01:52:37 -0300

    Version 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <h1>Version: 2.0</h1>
       </header>
       <MainContent />
     </>
