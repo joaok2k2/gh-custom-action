0c1a057c081532d5165c7433ccd8303fef9ee004
joaok2k2 <joak2k2@gmail.com>
2024-03-11 00:14:08 -0300

    Fix environment variables vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
+dotenv.config();
 export default defineConfig({
   plugins: [react()],
   test: {
@@ -11,4 +13,7 @@ export default defineConfig({
     reporters: ['json'],
     outputFile: 'test.json'
   },
+  define: {
+    'process.env': process.env
+  }
 });
