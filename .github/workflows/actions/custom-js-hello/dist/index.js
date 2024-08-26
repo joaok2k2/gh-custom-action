83bbc2738f58c00216cef4cc592aebf16f2b5afb
joaok2k2 <joak2k2@gmail.com>
2024-08-26 01:49:05 -0300

    Custom Simple Action using JS and builded modules using ncc 6

--- .github/workflows/actions/custom-js-hello/dist/index.js
+++ .github/workflows/actions/custom-js-hello/dist/index.js
@@ -31094,28 +31094,26 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(3370);
 const github = __nccwpck_require__(289);
 
-
 try {
-  const name = core.getInput("who_to_greet");
-
   core.debug("Debug Message");
   core.warning("Warning message");
   core.error("Error message");
 
+  const name = core.getInput("who_to_greet");
+
   console.log(`Hello ${name}`);
+
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
   core.exportVariable("HELLO_TIME", time);
 
-  core.startGroup("Logging githuyb context");
+  core.startGroup("Logging github context");
   console.log(JSON.stringify(github.context, null, 2));
   core.endGroup();
-  
 } catch (error) {
   core.setFailed(error.message);
 }
-
 })();
 
 module.exports = __webpack_exports__;
