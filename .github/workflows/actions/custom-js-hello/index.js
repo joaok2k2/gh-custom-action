c8cc63885edc2412d6f0b4586c7ca07de3ba8788
joaok2k2 <joak2k2@gmail.com>
2024-08-26 02:04:10 -0300

    Custom Simple Action using JS and builded modules using ncc 8

--- .github/workflows/actions/custom-js-hello/index.js
+++ .github/workflows/actions/custom-js-hello/index.js
@@ -1,15 +1,14 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 try {
+  const name = core.getInput("who_to_greet");
+
   core.debug("Debug Message");
   core.warning("Warning message");
   core.error("Error message");
 
-  const name = core.getInput("who_to_greet");
-
   console.log(`Hello ${name}`);
-
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
@@ -18,6 +17,7 @@ try {
   core.startGroup("Logging github context");
   console.log(JSON.stringify(github.context, null, 2));
   core.endGroup();
+  
 } catch (error) {
   core.setFailed(error.message);
 }
